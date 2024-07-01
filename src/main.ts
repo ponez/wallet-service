fb848af5aec77e676148e3442bf97327fbac076e
ponez <mohammadmahdi9898@gmail.com>
2024-07-02 03:13:23 +0330

    added global pipe and microservice example

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: 8877,
+    },
+  });
+
+  await app.startAllMicroservices();
   await app.listen(3000);
 }
 bootstrap();
