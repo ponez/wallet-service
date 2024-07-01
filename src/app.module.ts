03caf4746cb158d1607f3b77f00c88f02c04a8ce
ponez <mohammadmahdi9898@gmail.com>
2024-07-02 03:12:32 +0330

    updated app module with user/transaction service and imported graphql / typeorm servcie

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,47 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { GraphQLError, GraphQLFormattedError } from 'graphql/error';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UserModule } from '@user/user.module';
+import { TransactionModule } from '@transaction/transaction.module';
+import { TypeOrmDynamicModule } from '@typeorm-module/typeorm.module';
+import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { LoggingInterceptor } from '@logger-interceptor/logger';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: true,
+      buildSchemaOptions: {
+        dateScalarMode: 'timestamp',
+      },
+      formatError: (error: GraphQLError) => {
+        const graphQLFormattedError: GraphQLFormattedError = {
+          message: error?.message,
+          extensions: error.extensions,
+        };
+        return graphQLFormattedError;
+      },
+    }),
+    TypeOrmDynamicModule.forRoot(),
+    UserModule,
+    TransactionModule,
+  ],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
